7d58a7a34d7dfb5c14927b4bfb124bc3f9f24727
Sinisa Stevanovic <sinisahamburg97@gmail.com>
2025-01-26 14:05:31 +0100

    Add debug guard for debug settings

--- src/common.h
+++ src/common.h
@@ -4,9 +4,11 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#ifdef DEBUG
 #define DEBUG_PRINT_CODE
 //#define DEBUG_TRACE_EXECUTION // TODO: Disable in prod
 //#define DEBUG_STRESS_GC
 //#define DEBUG_LOG_GC
+#endif
 
 #define UINT8_COUNT (UINT8_MAX + 1)
